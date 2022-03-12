f07fbb435a91ec94a747ff6c3e2f77cbb23c95d4
Ivan Nikiforov <Travyanoi@users.noreply.github.com>
2022-03-12 19:59:35 +0700

    Upgrade main

--- main.c
+++ main.c
@@ -5,12 +5,18 @@ int sum(int a, int b)
     return a + b;
 }
 
+int minus(int a, int b)
+{
+    return a - b;
+}
+
 int main()
 {
     printf("Hello World!\n");
     int first = 55;
     int second = 345;
     sum(first, second);
+    minus(first, second);
 
     return 0;
 }
